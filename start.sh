c2cb37db44daded9075b466fb6636e973b66b16f
nullxz <nusara@furnu.org>
2023-12-31 02:38:38 +0700

    Update start.sh
    Change from bash to sh

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/sh
+# screen or tmux
+MODE="screen"
 # MyServer_1_7_9
 SESSION_NAME=""
 # java -jar -Xmx4G server.jar -nogui
@@ -6,29 +8,42 @@ COMMAND=""
 # /home/peipei/minecraft-server
 WORK_DIR=""
 
+if [ -z "$SESSION_NAME" ] || [ -z "$COMMAND" ] || [ -z "$WORK_DIR" ] || [ "$MODE" != "screen" ] && [ "$MODE" != "tmux" ]; then
+    echo "\033[1;31m[WARN] String is Empty"
+    exit 1
+fi
 
-if [[ -z $SESSION_NAME || -z $COMMAND || -z $WORK_DIR ]]; then
-	echo -e "\033[1;31m[WARN] String is Empty"
-	(exit 1)
-	exit
+cd "$WORK_DIR"
+echo "\033[1;33m[INFO] \033[1;37mEnter to: $(pwd)"
+if [ "$1" = "debug" ]; then
+    echo "\033[1;31m[DEBUG]: Start Minecraft Server IN DEBUG MODE (NO screen or tmux)"
+    $COMMAND
+    exit 0
 fi
-cd $WORK_DIR
-echo -e "\033[1;33m[INFO] \033[1;37mEnter to WORK_DIR: `pwd`"
-if [[ $1 == "debug" ]]; then
-	echo -e "\033[1;31m[DEBUG]: Start Minecraft Server IN DEBUG MODE (NO tmux)"
-	$COMMAND
-	(exit 0)
-	exit
+
+echo "\033[1;33m[INFO] \033[1;32mStart Minecraft Server IN $MODE \033[1;31m"
+case $MODE in
+    tmux)
+        tmux kill-session -t "$SESSION_NAME"
+        tmux new-session -d -s "$SESSION_NAME" $COMMAND
+        ;;
+    screen)
+        screen -dmS "$SESSION_NAME" $COMMAND
+        ;;
+esac
+
+if [ $? -ne 0 ]; then
+    echo "\033[1;31m[WARN] ERROR Exit..."
+    exit 1
 fi
-echo -e "\033[1;33m[INFO] \033[1;32mStart Minecraft Server IN NORMAL MODE \033[1;31m"
-tmux kill-session -t $SESSION_NAME
-tmux new-session -d -s $SESSION_NAME $COMMAND
-if [[ $? != 0 ]]; then
-	echo -e "\033[1;31m[WARN] ERROR"
-	(exit 1)
-	exit
-fi
-echo -e "\033[1;33m[INFO] \033[1;37mHow to control a Minecraft server"
-echo -e "\033[1;33m[INFO] \033[1;37mUse: tmux a -t SESSION_NAME"
-(exit 0)
-exit
+
+echo "\033[1;33m[INFO] \033[1;37mHow to control a Minecraft server"
+case $MODE in
+    tmux)
+        echo "\033[1;33m[INFO] \033[1;37mUse: tmux a -t $SESSION_NAME"
+        ;;
+    screen)
+        echo "\033[1;33m[INFO] \033[1;37mUse: screen -r $SESSION_NAME"
+        ;;
+esac
+exit 0
