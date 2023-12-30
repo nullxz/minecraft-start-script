590ffee7aa11673bfeb1f62314f55b528d9c4c20
nullxz <nusara@furnu.org>
2023-12-31 01:31:17 +0700

    Update start.sh
    kill tmux session before restarting minecraft server

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# MyServer1.7.9
+# MyServer_1_7_9
 SESSION_NAME=""
 # java -jar -Xmx4G server.jar -nogui
 COMMAND=""
@@ -15,13 +15,14 @@ fi
 cd $WORK_DIR
 echo -e "\033[1;33m[INFO] \033[1;37mEnter to WORK_DIR: `pwd`"
 if [[ $1 == "debug" ]]; then
-	echo -e "\033[1;31m[DEBUG]: Start Minecraft Server IN DEBUG MODE (NO TMUX)"
+	echo -e "\033[1;31m[DEBUG]: Start Minecraft Server IN DEBUG MODE (NO tmux)"
 	$COMMAND
 	(exit 0)
 	exit
 fi
 echo -e "\033[1;33m[INFO] \033[1;32mStart Minecraft Server IN NORMAL MODE \033[1;31m"
-/bin/tmux new-session -d -s $SESSION_NAME $COMMAND
+tmux kill-session -t $SESSION_NAME
+tmux new-session -d -s $SESSION_NAME $COMMAND
 if [[ $? != 0 ]]; then
 	echo -e "\033[1;31m[WARN] ERROR"
 	(exit 1)
